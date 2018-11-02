1735709878ec08719e62d615d62ac7b123318681
Thoopalli <amarnath.thoopalli@capgemini.com>
2018-11-02 15:37:04 +0530

    basic_controller_layer

--- src/main/java/com/capgemini/employee/controller/Controller.java
+++ src/main/java/com/capgemini/employee/controller/Controller.java
@@ -1,5 +1,32 @@
 package com.capgemini.employee.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.capgemini.employee.domain.Employee;
+import com.capgemini.employee.service.EmployeeService;
+
+@RestController
 public class Controller {
+
+	private EmployeeService employeeService;
+
+	@Autowired
+	public Controller(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public ResponseEntity<?> saveEmp(Employee employee) {
+
+		Employee empObj = employeeService.saveEmp(employee);
+
+		return new ResponseEntity<Employee>(empObj, HttpStatus.CREATED);
+
+	}
 
 }
