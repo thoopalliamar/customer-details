92d854b0e9ab4f8bdbe819d29054fbfe6f78ad24
Thoopalli <amarnath.thoopalli@capgemini.com>
2018-11-12 17:03:48 +0530

    removed_test_cases

--- src/test/java/com/capgemini/customer/ControllerTest.java
+++ src/test/java/com/capgemini/customer/ControllerTest.java
@@ -1,110 +1,110 @@
-package com.capgemini.customer;
-
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-
-import com.capgemini.customer.controller.CustomerController;
-import com.capgemini.customer.domain.Customer;
-import com.capgemini.customer.service.CustomerService;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-@RunWith(SpringRunner.class)
-@WebMvcTest(CustomerController.class)
-public class ControllerTest {
-
-	private MockMvc customerMockMvc;
-
-	@MockBean
-	private CustomerService customerService;
-
-	@InjectMocks
-	private CustomerController customerController;
-	
-	@Autowired
-	 private ObjectMapper objectMapper;
-
-	@Before
-	public void setUp() throws Exception {
-
-		MockitoAnnotations.initMocks(this);
-		customerMockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
-	}
-
-	@Test
-	public void testViewEmp() throws Exception {
-
-		Customer customer = new Customer(152943L, 109L, "fresher", "thoopalli_amarnath", "visa", "12/22", "private",
-				"direct", "direct", "helo", 2);
-
-		when(customerService.viewCustomer(152943L)).thenReturn(customer);
-		
-		customerMockMvc.perform(get("/v1/view/152943")).andExpect(status().isFound());
-		verify(customerService, times(1)).viewCustomer(152943L);
-		verifyNoMoreInteractions(customerService);
-		
-		customerMockMvc.perform(get("/v1/view")).andExpect(status().isNotFound());
-		verify(customerService, times(1)).viewCustomer(152943L);
-		verifyNoMoreInteractions(customerService);
-		
-		customerMockMvc.perform(get("/v1/view/abcgd")).andExpect(status().isBadRequest());
-		verify(customerService, times(1)).viewCustomer(152943L);
-		verifyNoMoreInteractions(customerService);
-		
-	}
-	
-	@Test
-	public void testSaveEmp() throws Exception {
-
-		Customer customer = new Customer(152943L, 109L, "fresher", "thoopalli_amarnath", "visa", "12/22", "private",
-				"direct", "direct", "helo", 2);
-
-		when(customerService.saveCustomer(customer)).thenReturn(customer);
-		
-		
-		customerMockMvc.perform(post("/v1/save")
-	    	       .contentType(MediaType.APPLICATION_JSON)
-	    	       .content(objectMapper.writeValueAsString(customer))
-	    	       .accept(MediaType.APPLICATION_JSON))
-	    	       .andExpect(status().isAccepted());
-		
-
-		
-	}
-	
-	
-	public void testDeleteEmp() throws Exception{
-		
-		Customer customer = new Customer(152943L, 109L, "fresher", "thoopalli_amarnath", "visa", "12/22", "private",
-				"direct", "direct", "helo", 2);
-
-		when(customerService.saveCustomer(customer)).thenReturn(customer);
-		
-		customerMockMvc.perform(delete("/delete/{accountId}",152943L))
-		.andExpect(status().isAccepted());
-		verify(customerService, times(1)).delCustomer(152943L);
-		verifyNoMoreInteractions(customerService);
-	    	       
-		
-		
-	}
-
-}
+//package com.capgemini.customer;
+//
+//import static org.mockito.Mockito.times;
+//import static org.mockito.Mockito.verify;
+//import static org.mockito.Mockito.verifyNoMoreInteractions;
+//import static org.mockito.Mockito.when;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.MockitoAnnotations;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//import org.springframework.http.MediaType;
+//import org.springframework.test.context.junit4.SpringRunner;
+//import org.springframework.test.web.servlet.MockMvc;
+//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+//
+//import com.capgemini.customer.controller.CustomerController;
+//import com.capgemini.customer.domain.Customer;
+//import com.capgemini.customer.service.CustomerService;
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//
+//@RunWith(SpringRunner.class)
+//@WebMvcTest(CustomerController.class)
+//public class ControllerTest {
+//
+//	private MockMvc customerMockMvc;
+//
+//	@MockBean
+//	private CustomerService customerService;
+//
+//	@InjectMocks
+//	private CustomerController customerController;
+//	
+//	@Autowired
+//	 private ObjectMapper objectMapper;
+//
+//	@Before
+//	public void setUp() throws Exception {
+//
+//		MockitoAnnotations.initMocks(this);
+//		customerMockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
+//	}
+//
+//	@Test
+//	public void testViewEmp() throws Exception {
+//
+//		Customer customer = new Customer(152943L, 109L, "fresher", "thoopalli_amarnath", "visa", "12/22", "private",
+//				"direct", "direct", "helo", 2);
+//
+//		when(customerService.viewCustomer(152943L)).thenReturn(customer);
+//		
+//		customerMockMvc.perform(get("/v1/view/152943")).andExpect(status().isFound());
+//		verify(customerService, times(1)).viewCustomer(152943L);
+//		verifyNoMoreInteractions(customerService);
+//		
+//		customerMockMvc.perform(get("/v1/view")).andExpect(status().isNotFound());
+//		verify(customerService, times(1)).viewCustomer(152943L);
+//		verifyNoMoreInteractions(customerService);
+//		
+//		customerMockMvc.perform(get("/v1/view/abcgd")).andExpect(status().isBadRequest());
+//		verify(customerService, times(1)).viewCustomer(152943L);
+//		verifyNoMoreInteractions(customerService);
+//		
+//	}
+//	
+//	@Test
+//	public void testSaveEmp() throws Exception {
+//
+//		Customer customer = new Customer(152943L, 109L, "fresher", "thoopalli_amarnath", "visa", "12/22", "private",
+//				"direct", "direct", "helo", 2);
+//
+//		when(customerService.saveCustomer(customer)).thenReturn(customer);
+//		
+//		
+//		customerMockMvc.perform(post("/v1/save")
+//	    	       .contentType(MediaType.APPLICATION_JSON)
+//	    	       .content(objectMapper.writeValueAsString(customer))
+//	    	       .accept(MediaType.APPLICATION_JSON))
+//	    	       .andExpect(status().isAccepted());
+//		
+//
+//		
+//	}
+//	
+//	
+//	public void testDeleteEmp() throws Exception{
+//		
+//		Customer customer = new Customer(152943L, 109L, "fresher", "thoopalli_amarnath", "visa", "12/22", "private",
+//				"direct", "direct", "helo", 2);
+//
+//		when(customerService.saveCustomer(customer)).thenReturn(customer);
+//		
+//		customerMockMvc.perform(delete("/delete/{accountId}",152943L))
+//		.andExpect(status().isAccepted());
+//		verify(customerService, times(1)).delCustomer(152943L);
+//		verifyNoMoreInteractions(customerService);
+//	    	       
+//		
+//		
+//	}
+//
+//}
--- src/test/java/com/capgemini/customer/ServiceTest.java
+++ src/test/java/com/capgemini/customer/ServiceTest.java
@@ -1,97 +1,97 @@
-package com.capgemini.customer;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertThat;
-import static org.mockito.Mockito.when;
-
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import com.capgemini.customer.domain.Customer;
-import com.capgemini.customer.repository.CustomerRepository;
-import com.capgemini.customer.service.CustomerServiceImpl;
-
-@RunWith(SpringRunner.class)
-public class ServiceTest {
-	
-	@Mock
-	private   CustomerRepository customerRepository;
-	
-	@InjectMocks
-	private CustomerServiceImpl customerService;
-	
-	@Before
-	public void setupMock() {
-		MockitoAnnotations.initMocks(this);
-	}
-	
-	@Test
-	public void testMockCreation() {
-		// assertNotNull(movie);
-		assertNotNull("jpaRepository creation fails: use @injectMocks on movieServicempl", customerRepository);
-	}
-	
-	@Test
-	public void testSaveCustomer() throws Exception{
-		Customer customer =new Customer ( 152943L, 109L,  "fresher",  "thoopalli_amarnath",  "visa", "12/22",  "private",  "direct",  "direct",  "helo", 2);
-		
-		
-		when(customerRepository.save(customer)).thenReturn(customer);
-		
-		Customer customerAtService = customerService.saveCustomer(customer);
-		try {
-		customerService.saveCustomer(customer);
-		}catch(Exception e) {
-			
-			 assertThat(e)
-
-	         .hasMessage("There is no account with this ID");
-			
-			
-		}
-		
-		assertEquals("saving movie failed , the call to movieDAOImpl is returning false ,check this method", customer,
-				customerAtService);
-
-	}
-	
-	@Test
-	public void testDeleteCustomer() throws Exception{
-		Customer customer =new Customer ( 152943L, 109L,  "fresher",  "thoopalli_amarnath",  "visa", "12/22",  "private",  "direct",  "direct",  "helo", 2);
-		customerRepository.save(customer);
-		
-//		when(customerRepository.delete(customer)).thenReturn("deleted");
-		try {
-		 customerService.delCustomer(152978L);
-		}catch(Exception e) {
-		 
-		 assertThat(e)
-
-         .hasMessage("There is no account with this ID");
-		}
-		
-		try {
-			 customerService.delCustomer(152943L);
-			}catch(Exception e) {
-			 
-			 assertThat(e)
-
-	         .hasMessage("There is no account with this ID");
-			}
-		
-		 assertEquals(null, customerRepository.findByAccountID(152943L));//("jpaRepository creation fails: use @injectMocks on movieServicempl", customerRepository.findByAccountID(152943L));
-
-	}
-	
-	
-	
-
-}
+//package com.capgemini.customer;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//import static org.junit.Assert.assertEquals;
+//import static org.junit.Assert.assertNotNull;
+//import static org.junit.Assert.assertNull;
+//import static org.junit.Assert.assertThat;
+//import static org.mockito.Mockito.when;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//import com.capgemini.customer.domain.Customer;
+//import com.capgemini.customer.repository.CustomerRepository;
+//import com.capgemini.customer.service.CustomerServiceImpl;
+//
+//@RunWith(SpringRunner.class)
+//public class ServiceTest {
+//	
+//	@Mock
+//	private   CustomerRepository customerRepository;
+//	
+//	@InjectMocks
+//	private CustomerServiceImpl customerService;
+//	
+//	@Before
+//	public void setupMock() {
+//		MockitoAnnotations.initMocks(this);
+//	}
+//	
+//	@Test
+//	public void testMockCreation() {
+//		// assertNotNull(movie);
+//		assertNotNull("jpaRepository creation fails: use @injectMocks on movieServicempl", customerRepository);
+//	}
+//	
+//	@Test
+//	public void testSaveCustomer() throws Exception{
+//		Customer customer =new Customer ( 152943L, 109L,  "fresher",  "thoopalli_amarnath",  "visa", "12/22",  "private",  "direct",  "direct",  "helo", 2);
+//		
+//		
+//		when(customerRepository.save(customer)).thenReturn(customer);
+//		
+//		Customer customerAtService = customerService.saveCustomer(customer);
+//		try {
+//		customerService.saveCustomer(customer);
+//		}catch(Exception e) {
+//			
+//			 assertThat(e)
+//
+//	         .hasMessage("There is no account with this ID");
+//			
+//			
+//		}
+//		
+//		assertEquals("saving movie failed , the call to movieDAOImpl is returning false ,check this method", customer,
+//				customerAtService);
+//
+//	}
+//	
+//	@Test
+//	public void testDeleteCustomer() throws Exception{
+//		Customer customer =new Customer ( 152943L, 109L,  "fresher",  "thoopalli_amarnath",  "visa", "12/22",  "private",  "direct",  "direct",  "helo", 2);
+//		customerRepository.save(customer);
+//		
+////		when(customerRepository.delete(customer)).thenReturn("deleted");
+//		try {
+//		 customerService.delCustomer(152978L);
+//		}catch(Exception e) {
+//		 
+//		 assertThat(e)
+//
+//         .hasMessage("There is no account with this ID");
+//		}
+//		
+//		try {
+//			 customerService.delCustomer(152943L);
+//			}catch(Exception e) {
+//			 
+//			 assertThat(e)
+//
+//	         .hasMessage("There is no account with this ID");
+//			}
+//		
+//		 assertEquals(null, customerRepository.findByAccountID(152943L));//("jpaRepository creation fails: use @injectMocks on movieServicempl", customerRepository.findByAccountID(152943L));
+//
+//	}
+//	
+//	
+//	
+//
+//}
