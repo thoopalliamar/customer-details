1c147dc30dc96225e3a2595f2c562bd9eb94645b
Thoopalli <amarnath.thoopalli@capgemini.com>
2018-11-12 17:56:47 +0530

    uncommented_repo_test

--- src/test/java/com/capgemini/customer/RepositoryTest.java
+++ src/test/java/com/capgemini/customer/RepositoryTest.java
@@ -1,48 +1,48 @@
-//package com.capgemini.customer;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.junit.Assert.assertThat;
-//
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import com.capgemini.customer.domain.Customer;
-//import com.capgemini.customer.repository.CustomerRepository;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
-//@AutoConfigureTestDatabase(replace = Replace.NONE)
-//public class RepositoryTest {
-//	
-//	private   CustomerRepository customerRepository;
-//	
-//	
-//
-//	public RepositoryTest() {
-//		super();
-//	}
-//
-//	@Autowired
-//	public void setRepositoryTest(CustomerRepository customerRepository) {
-//		this.customerRepository = customerRepository;
-//	}
-//	
-//	@Test
-//	public void testSaveCustomer() throws Exception{
-//		customerRepository.save(new Customer ( 152943L, 109L,  "fresher",  "thoopalli_amarnath",  "visa", "12/22",  "private",  "direct",  "direct",  "helo", 2));
-//		
-//		Customer customer = customerRepository.findByAccountID(152943L);
-//		
-//		assertThat(customer.getBankType()).isEqualTo("direct");
-//	}
-//	
-//
-//	
-//	
-//
-//}
+package com.capgemini.customer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.capgemini.customer.domain.Customer;
+import com.capgemini.customer.repository.CustomerRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+public class RepositoryTest {
+	
+	private   CustomerRepository customerRepository;
+	
+	
+
+	public RepositoryTest() {
+		super();
+	}
+
+	@Autowired
+	public void setRepositoryTest(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+	
+	@Test
+	public void testSaveCustomer() throws Exception{
+		customerRepository.save(new Customer ( 152943L, 109L,  "fresher",  "thoopalli_amarnath",  "visa", "12/22",  "private",  "direct",  "direct",  "helo", 2));
+		
+		Customer customer = customerRepository.findByAccountID(152943L);
+		
+		assertThat(customer.getBankType()).isEqualTo("direct");
+	}
+	
+
+	
+	
+
+}
